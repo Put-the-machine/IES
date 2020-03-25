e9ca86088dcd1fc2efa2640ca8c56912bbb814ab
uolken <palashevskiymaxim@gmail.com>
2020-03-25 22:28:50 +0400

    Добавлены профили сборки Maven

--- backend/IES/src/main/java/ies/Application.java
+++ backend/IES/src/main/java/ies/Application.java
@@ -2,8 +2,10 @@ package ies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application.yaml")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
